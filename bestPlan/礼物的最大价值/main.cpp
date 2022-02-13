817fb5d50f59e20f7bae970209fa6fb5f2ef1822
Cui-Yancy <813158377@qq.com>
2022-02-13 14:05:34 +0800

    add dynamicProg exam 2

---
+++ bestPlan/礼物的最大价值/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <vector>
+#include <iostream>
+using namespace std;
+
+//求最大值路径
+//从左上角到第(i,j)点的最大值路径为：从(i-1,j)和(i,j-1)两个点的最大值路径中选取最大值，加上(i,j)的值即可
+
+class Solution {
+public:
+    int maxValue(vector<vector<int>>& grid) {
+        //初始化第一列
+        for(int i=1; i<grid.size(); i++)
+            grid[i][0] += grid[i-1][0];
+
+        //初始化第一行
+        for(int i=1; i<grid[0].size(); i++)
+            grid[0][i] += grid[0][i-1];
+
+        //求其它行列的值
+        for(int i=1; i<grid.size() ;i++)
+            for(int j = 1; j <grid[0].size(); j++)
+                grid[i][j] += (grid[i][j-1]>grid[i-1][j]?grid[i][j-1]:grid[i-1][j]);
+        return grid[grid.size()-1][grid[0].size()-1];
+    }
+};
---
+++ bestPlan/连续子数组和的最大值/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = nums[0];  //第i个元素之前的 包含第i-1个元素的子序列和的最大值
+        int ret = sum;      //记录最大的子序列和
+        for(int i=1; i<nums.size(); i++)
+        {
+            if(sum<=0) sum = nums[i];   //如果之前的子序列最大值为非正数，就不需要再加上之前的
+            else sum += nums[i];        //否则就将最大值加上
+            ret = ret>sum?ret:sum;      //新的最大值和旧的最大值取最大的记录下来
+        }
+        return ret;
+    }
+};
