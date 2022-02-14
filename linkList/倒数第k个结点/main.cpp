57739c5a8d4a226ebfc9631864df7f55632a3105
Cui-Yancy <813158377@qq.com>
2022-02-14 13:13:10 +0800

    add 2 linkList exams

---
+++ linkList/倒数第k个结点/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+#include <stack>
+
+// Definition for singly-linked list.
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+
+class Solution {
+public:
+    ListNode* getKthFromEnd(ListNode* head, int k) {
+        // stack<ListNode*> stk;
+        // ListNode* p=head;
+        // while(p)
+        // {
+        //     stk.push(p);
+        //     p = p->next;
+        // }
+        // if(k>stk.size()) return NULL;
+        // for(int i=0; i<k-1; i++)
+        //     stk.pop();
+        // return stk.top();
+
+        ListNode* p = head;
+        int num = 0;
+        while(p)
+        {
+            p = p->next;
+            num ++;
+        }
+        if(k>num) return NULL;
+        p = head;
+        for(int i=0; i<(num-k); i++)
+            p = p->next;
+        return p;
+    }
+};
---
+++ linkList/删除节点/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+// Definition for singly-linked list.
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+
+class Solution {
+public:
+    ListNode* deleteNode(ListNode* head, int val) {
+        if(!head) return NULL;                  //输入空链表时，返回空
+        if(head->val==val) return head->next;   //首结点为删除值时，直接返回next
+        ListNode* p = head;
+        ListNode* pri = NULL;
+        while(p)
+        {
+            if(p->val==val)
+                pri->next = p->next;
+            pri = p;
+            p = p->next;
+        }
+        return head;
+    }
+};
