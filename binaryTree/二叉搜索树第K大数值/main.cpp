0c5e8f4fddffe2835722776172fa0fe987175ce2
Cui-Yancy <813158377@qq.com>
2022-02-17 22:16:55 +0800

    add binaryTree exam for First and Mid Traverse

---
+++ binaryTree/二叉搜索树第K大数值/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+//
+#include <vector>
+
+class Solution {
+public:
+    int kthLargest(TreeNode* root, int k) {
+        vector<int> v = lastSearch(root);
+        return v[k-1];
+    }
+
+    vector<int> lastSearch(TreeNode* root)
+    {
+        if(!root) return {};
+        vector<int> v(lastSearch(root->right));
+        v.push_back(root->val);
+        vector<int> tmp = lastSearch(root->left);
+        v.insert(v.end(),tmp.begin(),tmp.end());
+    }
+};
---
+++ binaryTree/搜索目标值总和路径/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+#include <stack>
+#include <vector>
+
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int target) {
+        firstReverse(root,target);
+        return ret;
+    }
+
+    vector<vector<int>> ret;
+    vector<int> tmp;
+    void firstReverse(TreeNode* root, int sum)
+    {
+        if(!root) return;
+        sum -= root->val;
+        tmp.push_back(root->val);
+        if(sum==0 && !root->left && !root->right)
+        {
+            ret.push_back(tmp);
+        }
+        firstReverse(root->left,sum);
+        firstReverse(root->right,sum);
+        tmp.pop_back();
+    }
+};
