822f6e5b40711bd15aa46f0dee500c4f03f51099
Cui-Yancy <813158377@qq.com>
2022-02-19 18:04:22 +0800

    add 2 sort exams

---
+++ sort/扑克牌中的顺子/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+#include <vector>
+
+//从若干副扑克牌中随机抽 5 张牌，判断是不是一个顺子,即这5张牌是不是连续的。
+//2～10为数字本身，A为1，J为11，Q为12，K为13
+//而大、小王为 0 ，可以看成任意数字。A 不能视为 14。
+
+//考虑使用哈希表，记录每张牌的出现次数，以及除了0的最小值和最大值
+//从最小值开始，到最大值，如果缺牌，用0代替
+//如果出现两张以上相同的牌，返回假
+//最后看大小王的数量，如果小于0说明缺牌太多返回假，否则返回真
+
+class Solution {
+public:
+    bool isStraight(vector<int>& nums) {
+        char a[14] = {0,};
+        int max = 0, min = 14;
+        for(int i=0;i<5;i++)
+        {
+            a[nums[i]]++;
+            if(nums[i]<min && min) min = nums[i];
+            if(nums[i]>max) max = nums[i];
+        }
+        for(int i=min;i<=max;i++)
+        {
+            if(a[i]==0) a[0]--;
+            if(a[i]>1) return false;
+        }
+        if(a[0]<0) return false;
+        return true;
+    }
+};
---
+++ sort/拼接最小数字/main.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using namespace std;
+#include <vector>
+#include <string>
+#include <stdlib.h>
+
+//两个数字形成的字符串为 a b
+//如果 a+b>b+a 说明应该将b放在a前边 a较大
+
+class Solution {
+public:
+    string minNumber(vector<int>& nums) {
+        string str = "";
+        vector<string> v;
+        for(int i=0;i<nums.size();i++)
+            v.push_back(to_string(nums[i]));
+        int n=v.size();
+        //sort(v);
+        quickSort(v,0,n-1);
+        for(int i=0;i<n;i++)
+            str+=v[i];
+        return str;
+        stoi(str);
+    }
+
+    void sort(vector<string> &v)
+    {
+        int len = v.size();
+        for(int i=0;i<len-1;i++)
+        {
+            bool noExchange = true;
+            for(int j=0;j<len-i-1;j++)
+            {
+                if((v[j]+v[j+1])>(v[j+1]+v[j])) {swap(v[j],v[j+1]);noExchange = false;}
+            }
+            if(noExchange) break;
+        }
+    }
+
+    void quickSort(vector<string> &v, int low, int high)
+    {
+        if(low>=high) return;   //这里的判断条件必须是大于等于，如果本身就是递增序列
+        int x=low, y=high;      //第一次快排之后low=high=0，子序列快排区间就会出现[0,-1]
+        string mid = v[low];
+        while(low<high)
+        {
+            while(low<high && (v[high]+mid)>=(mid+v[high])) high--;
+            if(low==high) break;
+            swap(v[low],v[high]);
+            low++;
+            while(low<high && (v[low]+mid)<=(mid+v[low])) low++;
+            if(low==high) break;
+            swap(v[low],v[high]);
+            high--;
+        }
+        v[low] = mid;
+        quickSort(v,x,low-1);
+        quickSort(v,low+1,y);
+    }
+};
