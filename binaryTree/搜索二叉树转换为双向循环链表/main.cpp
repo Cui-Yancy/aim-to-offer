8b7f17842c7d671e123320b8751e8df332b9bdef
Cui-Yancy <813158377@qq.com>
2022-02-19 10:52:46 +0800

    add 2 exams

---
+++ binaryTree/搜索二叉树转换为双向循环链表/main.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+        left = NULL;
+        right = NULL;
+    }
+
+    Node(int _val, Node* _left, Node* _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+
+class Solution {
+public:
+    Node* treeToDoublyList(Node* root) {
+        if(!root) return NULL;
+        pre = NULL;
+        midTraverse(root);
+        head->left = pre;
+        pre->right = head;
+        return head;
+    }
+
+    Node* head, *pre;
+    void midTraverse(Node* root)
+    {
+        if(!root) return;
+        midTraverse(root->left);
+        if(!pre) head = root;       //第一次访问pre时，是最小结点，此时记录头结点
+        else {root->left = pre; pre->right = root;}
+        pre = root;
+        midTraverse(root->right);
+    }
+};
---
+++ search/查找机器人的运动范围/main.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+using namespace std;
+
+//m行n列的矩阵，机器人从[0,0出发]
+//坐标各位之和不大于k
+//问能进入几个格子
+
+class Solution {
+public:
+    int movingCount(int m, int n, int k) {
+        sumL = k;
+        xL = m;
+        yL = n;
+        ret = 0;
+        visitedInit();
+        BFS(0,0);
+        return ret;
+    }
+
+    int ret;
+    int xL, yL, sumL;
+    bool ** visited;
+
+    //初始化标记数组，用于标记坐标是否被访问过
+    void visitedInit()
+    {
+        visited = new bool*[xL];
+        for(int i=0;i<xL;i++)
+        {
+            visited[i] = new bool[yL];
+        }
+        for(int i=0;i<xL;i++)
+            for(int j=0;j<yL;j++)
+                visited[i][j] = false;
+    }
+
+    void BFS(int x, int y)
+    {
+        //坐标在范围之外，或者坐标各位之和不在范围之内，或者坐标点已经访问过来
+        if(x<0 || x>=xL || y<0 || y>=yL || visited[x][y] ||  !inLimit(x,y)) return;
+        ret++;
+        visited[x][y] = true;
+        BFS(x+1,y);
+        BFS(x-1,y);
+        BFS(x,y+1);
+        BFS(x,y-1);
+    }
+
+    //用于判断坐标值是否在范围之内
+    bool inLimit(int x, int y)
+    {
+        int sum = 0;
+        while(x)
+        {
+            sum += (x%10);
+            x /= 10;
+        }
+        while(y)
+        {
+            sum += (y%10);
+            y /= 10;
+        }
+        return (sum<=sumL)?true:false;
+    }
+};
